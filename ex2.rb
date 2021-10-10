1f2f026469707144d6e18899a9549e7c2935ac8a
Oybek Kayumov <oybek.kayumov@gmail.com>
2021-10-10 20:26:56 +0500

    text errors

--- ex2.rb
+++ ex2.rb
@@ -1,8 +1,8 @@
 # A comment, this is so you can read your program later.
-# Anything after thr # is ignored by Ruby.
+# Anything after the # is ignored by Ruby.
 puts "I should have code like this." # and the comment after is ignored
 
-# You can also use a comment to "disable" or comment out a piece og code:
+# You can also use a comment to "disable" or comment out a piece of code:
 # puts "This won't run"
 
 puts "This will run"
