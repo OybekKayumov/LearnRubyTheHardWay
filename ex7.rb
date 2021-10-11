eb6690fd48d7178d348e70b5e5cfc58d00d63d89
Oybek Kayumov <oybek.kayumov@gmail.com>
2021-10-11 15:58:22 +0500

    ex7.rb to branch ex10

---
+++ ex7.rb
@@ -0,0 +1,29 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+puts "." * 30 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notic e how we are using print instead of puts here. change it to puts
+# and see what happens.
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+puts
+puts end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# this just is polite use of the terminal, try removing it
+puts
+puts # puts empty line
