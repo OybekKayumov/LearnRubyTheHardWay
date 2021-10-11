c9d772abc2a4e975499de31dfb0460523f1f60f6
Oybek Kayumov <oybek.kayumov@gmail.com>
2021-10-11 15:03:47 +0500

    ex5.rb to branch ex2

--- ex5.rb
+++ ex5.rb
@@ -1,18 +1,18 @@
-my_name = 'Oybek Kayumov'
-my_age = 47 # not a lie
-my_height = "175" #sm
-my_weight = 103 # kg
-my_eyes = "Brown"
-my_teeth = "White"
-my_hair = "Black"
+name = 'Oybek Kayumov'
+age = 47 # not a lie
+height = "175" #sm
+weight = 103 # kg
+eyes = "Brown"
+teeth = "White"
+hair = "Black"
 
-puts "Let's talk about %s." % my_name
-puts "He's %d santimeters tall." % my_height
-puts "He's %d kg heavy." % my_weight
+puts "Let's talk about %s." % name
+puts "He's %d santimeters tall." % height
+puts "He's %d kg heavy." % weight
 puts "Actually that's not too heavy." 
-puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
-puts "He's teeth are usually %s depending on coffee" % my_teeth
+puts "He's got %s eyes and %s hair." % [eyes, hair]
+puts "He's teeth are usually %s depending on coffee" % teeth
 
 # this line is tricky, try to get it exactly right
 puts "If I add %d, %d, and %d I get %d." % [
-my_age, my_height, my_weight, my_age.to_i + my_height.to_i + my_weight.to_i]
+age, height, weight, age.to_i + height.to_i + weight.to_i]
