42b011de44927e887d4df8a29ff0cb463b1f3699
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-17 21:27:12 +0500

    add b * 2

--- pry/complicated_array.rb
+++ pry/complicated_array.rb
@@ -5,7 +5,7 @@ complicated_array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 def puts_array(array)
   new_array = array.map do |a|
     a.map do |b|
-      binding.pry      
+      b * 2      
     end
   end
   puts new_array
@@ -13,3 +13,17 @@ end
 
 puts_array(complicated_array)
 
+#* output
+# 2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+
+# used commands:
+# exit, exit!
+# binding.pry
