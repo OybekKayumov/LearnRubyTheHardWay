87705fe23a0d4c9fa815e8ad68c965c328652998
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-19 19:10:58 +0500

    add before and after hooks

--- spec/before_and_after_spec.rb
+++ spec/before_and_after_spec.rb
@@ -1,4 +1,12 @@
 RSpec.describe "before and after hooks" do
+  before(:example) do
+    puts "Before example"
+  end
+
+  after(:example) do
+    puts "After example"
+  end
+
   it 'is just a random example' do
     expect(5 * 4).to eq(20) 
   end  
