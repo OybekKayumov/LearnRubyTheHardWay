181fcb8d2bf11395642d136cb58c7d7249d6f80e
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-19 18:27:45 +0500

    context and describe Methods are identical

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -1,20 +1,23 @@
 RSpec.describe "#even? method" do
   # it 'should return true if number is even'
-
   # it 'should return false if number is odd'
 
   #even? method with even number
-  describe "with even number" do
+  # describe "with even number" do
+  context "with even number" do
     it 'should return true' do
       expect(4.even?).to eq(true)
     end     
   end
   
   #even? method with odd number
-  describe "with odd number" do
+  # describe "with odd number" do
+  context "with odd number" do
     it 'should return false' do
       expect(5.even?).to eq(false)
     end
   end
   
 end
+
+#! context and describe are identical
