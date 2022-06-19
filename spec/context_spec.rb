43560f086b7a79d1cef052a4346d0e0cad4d7e86
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-19 18:20:31 +0500

    context Method and Nested Describes

---
+++ spec/context_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe "#even? method" do
+  # it 'should return true if number is even'
+
+  # it 'should return false if number is odd'
+
+  #even? method with even number
+  describe "with even number" do
+    
+  end
+  
+  #even? method with odd number
+  describe "with odd number" do
+    
+  end
+  
+end
