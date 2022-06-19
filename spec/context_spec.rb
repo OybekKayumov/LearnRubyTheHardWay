839a79cd9e1cb69f55a2e0554b679df7a5c82f55
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-19 18:22:24 +0500

    context Method and Nested Describes it should returns

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -5,12 +5,16 @@ RSpec.describe "#even? method" do
 
   #even? method with even number
   describe "with even number" do
-    
+    it 'should return true' do
+      expect(4.even?).to eq(true)
+    end     
   end
   
   #even? method with odd number
   describe "with odd number" do
-    
+    it 'should return false' do
+      expect(5.even?).to eq(false)
+    end
   end
   
 end
