3a1485dc716b83c8d3cec93a9ec7d65ac6977f4a
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-20 15:45:31 +0500

    add array while loop

---
+++ arrays/iteration_while_until.rb
@@ -0,0 +1,18 @@
+# Iteration over Array with while or until Loops
+animals = ["lion", 'zebra', 'baboon', 'cheetah']
+
+i = 0 
+while i < animals.length # 4
+  puts i
+  puts animals[i]
+  i +=1
+end
+
+# 0
+# lion
+# 1
+# zebra
+# 2
+# baboon
+# 3
+# cheetah
