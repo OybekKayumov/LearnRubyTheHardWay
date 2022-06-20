e5a652b14a750a1969384f9424878f044a7fb47c
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-20 16:28:27 +0500

    add sort in loop

--- arrays/reverse.rb
+++ arrays/reverse.rb
@@ -15,5 +15,27 @@ queue.reverse!
 p queue
 
 # [42, 23, 16, 15, 8, 4]
+puts
 
-#tood sort
+#todo sort
+number = [4, 2, 3, 1, 8, 7, 6, 5, 9]
+words = ["caterpillar", "kangaroo", "apple", "zebra"]
+Words = ["caterpillar", "kangaroo", "apple", "Zebra"]
+
+p number.sort
+p words.sort
+# [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# ["apple", "caterpillar", "kangaroo", "zebra"]
+
+p Words.sort
+#* ["Zebra", "apple", "caterpillar", "kangaroo"]
+
+#! will overwrite the original array object 
+# number.sort!
+# words.sort!
+
+puts
+p number.sort.reverse
+p words.sort.reverse
+# [9, 8, 7, 6, 5, 4, 3, 2, 1]
+# ["zebra", "kangaroo", "caterpillar", "apple"]
