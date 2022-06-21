67eb747b1e84413c8c5bc650e05c4fe9a36caf68
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-21 07:32:41 +0500

    add join string

--- strings/iterate.rb
+++ strings/iterate.rb
@@ -32,3 +32,37 @@ letters.each {|letter| puts "#{letter} is present"}
 # y is present
 
 #todo The .join Method on an Array
+
+names = ['Joe', 'Moe', 'Bob']
+p names.join
+# "JoeMoeBob"
+p names.join('-')
+# "Joe-Moe-Bob"
+
+puts
+p ['h', 'e', 'l', 'l', 'o'].join
+# "hello"
+
+puts
+def custom_join(arr, delimeter = "")
+  str = ""
+  last_element = arr.length - 1
+  arr.each_with_index do |elem, index|
+    str << elem 
+    # unless index == arr.length - 1 # last element
+    # if index != arr.length - 1 # last element
+      # str << delimeter   
+    # end
+    str << delimeter unless index == last_element  
+  end
+
+  str  
+end
+
+p custom_join(names)
+p custom_join(names, "-")
+# "JoeMoeBob"
+# "Joe-Moe-Bob", NOT "Joe-Moe-Bob-"
+
+p custom_join(names, "!---!")
+# "Joe!---!Moe!---!Bob"
