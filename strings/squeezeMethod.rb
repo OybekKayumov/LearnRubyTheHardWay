861c37effe0c39f2aada3085c4d5c0d0d6fecf2b
OybekKayumov <oybek.kayumov@gmail.com>
2022-06-22 05:17:10 +0500

    update squeeze method string

--- strings/squeezeMethod.rb
+++ strings/squeezeMethod.rb
@@ -21,8 +21,17 @@ puts
 sentence2 = "Thhe aardvarkk jummmped    over the fence!  ! !"
 
 def custom_squeeze(string)
+  final = ''
+  chars = string.split('') # = as "string.chars", returns an array of characters
 
-  
+  chars.each_with_index do |char, index|
+    if char == chars[index + 1]
+      next
+    else 
+      final << char
+    end
+  end
+  final
 end
 
 p custom_squeeze(sentence2)
