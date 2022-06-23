25efd57eaaea6cd7e50a669837aedbc2eea83c40
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-23 05:49:05 +0500

    add zip method

---
+++ arrays/zip.rb
@@ -0,0 +1,7 @@
+# The .zip Method on an Array
+
+names = ['Bob', 'Tom', 'John']
+registrations = [true, false, false]
+
+p names.zip(registrations)
+# [["Bob", true], ["Tom", false], ["John", false]]
