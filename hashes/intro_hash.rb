e1da30cc0506b75a6d0fa72fcc0e5d34b25089b2
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-24 04:54:45 +0500

    add empty hash

---
+++ hashes/intro_hash.rb
@@ -0,0 +1,13 @@
+# Intro to Hashes
+empty_hash = {}
+
+p empty_hash  # {}
+p empty_hash.class # Hash
+
+# 
+puts
+empty_array = []
+p empty_array   # []
+p empty_array.class # Array
+
+p empty_hash == empty_array # false
