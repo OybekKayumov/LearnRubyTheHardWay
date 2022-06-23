334c89700a7e571f4814487be76bc91226c8e926
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-23 05:28:20 +0500

    add flatten method

---
+++ arrays/flatten.rb
@@ -0,0 +1,22 @@
+# The .flatten Method on an Array
+# flattened method on an array removes all interior nested arrays within the main array.
+# It simply extracts their elements and puts them all in sequence and gives you back a single one dimensional array.
+
+registrations = [
+  ['Bob', 'Dan', 'Jimm'],
+  ['Rick', 'Susan', 'Molly'],
+  ['Tom', 'Sean', 'George']
+]
+
+a, b, c = registrations
+p a
+p b
+p c 
+# ["Bob", "Dan", "Jimm"]
+# ["Rick", "Susan", "Molly"]
+# ["Tom", "Sean", "George"]
+
+p registrations.flatten
+# ["Bob", "Dan", "Jimm", "Rick", "Susan", "Molly", "Tom", "Sean", "George"]
+
+
