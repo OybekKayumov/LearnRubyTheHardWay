1a806206032c26a400c826f5b0e54b3665aabc25
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-23 08:05:37 +0500

    add array insertion with ampersand & symbol

--- arrays/union.rb
+++ arrays/union.rb
@@ -41,9 +41,37 @@ def custom_subtraction(arr1, arr2)
   final = []
   
   arr1.each { |val| final << val unless arr2.include?(val) }
-
+  
   final
 end
 
 p custom_subtraction(a, b)
 # [2, 2, 3, 2, 3, 3]
+
+puts
+# todo Array Intersection with the Ampersand Symbol
+p [1, 1, 2, 3, 4, 5] & [1, 4, 5, 8, 9]
+# [1, 4, 5]
+
+# ampersand symbol is going to get back is an array of all the elements that are present in both of arrays.  
+# Notice that deduplication has also occurred, but includes one of them
+
+p [1, 1, 2, 3, 4, 5].& [1, 4, 5, 8, 9]
+p [1, 1, 2, 3, 4, 5].&([1, 4, 5, 8, 9])
+# [1, 4, 5]
+# [1, 4, 5]
+
+puts 
+a = [1, 1, 2, 3, 4, 5]
+b = [1, 4, 5, 8, 9]
+
+def custom_intersection(arr1, arr2)
+  final = []
+  
+  arr1.uniq.each { |elem| final << elem if arr2.include?(elem) }
+  
+  final
+end
+
+p custom_intersection(a, b)
+# [1, 4, 5]
