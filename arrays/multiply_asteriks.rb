6c9c16660c33a9cd6c10b958082c99ff67962449
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-23 07:01:57 +0500

    add multiply an array with astriks symbol

---
+++ arrays/multiply_asteriks.rb
@@ -0,0 +1,10 @@
+# Multiply an Array with Asterisk Symbol
+puts 4 * 3
+puts "Ruby " * 3
+# Ruby Ruby Ruby
+
+p [1, 2, 3] * 5
+# [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+p ['A', 'B', 'C'] * 4
+# ["A", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C"]
