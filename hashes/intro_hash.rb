de0a43fcef37160c6206a5a0e765066108b06025
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-24 04:59:43 +0500

    add hash: key-value

--- hashes/intro_hash.rb
+++ hashes/intro_hash.rb
@@ -1,4 +1,4 @@
-# Intro to Hashes
+#todo Intro to Hashes
 empty_hash = {}
 
 p empty_hash  # {}
@@ -11,3 +11,13 @@ p empty_array   # []
 p empty_array.class # Array
 
 p empty_hash == empty_array # false
+
+#todo Create Hash and Extract Values from Hashes by their Keys
+# key => value; keys are uniq, values can be duplicated
+nfl_roster = 
+{
+  "Tom Brady" => "New England Patriots",
+  "Tony Romo" => "Dallas Cowboys",
+  "Rob Gronkowski" => "New England Patriots"
+}
+
