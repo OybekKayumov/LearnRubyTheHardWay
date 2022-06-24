07c14182bcc8fb51037abc29f0a4d0e08259cb60
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-24 05:10:52 +0500

    add hash: key-value extract

--- hashes/intro_hash.rb
+++ hashes/intro_hash.rb
@@ -26,3 +26,20 @@ nba_roster =
   "Cleveland Cavaliers" => [ "LeBron James", "Kevin Love", "Kyrie Irving" ],
   "GS Warriors" => [ "Stephen Curry", "Klay Thomson", "Kevin Durant" ]  
 }
+
+# extrac
+p nfl_roster
+# {"Tom Brady"=>"New England Patriots", "Tony Romo"=>"Dallas Cowboys", "Rob Gronkowski"=>"New England Patriots"}
+
+puts
+p nfl_roster["Tony Romo"]
+# "Dallas Cowboys"
+p nfl_roster["Rob Gronkowski"]
+# "New England Patriots"
+
+puts
+p nba_roster["Cleveland Cavaliers"]
+# [ "LeBron James", "Kevin Love", "Kyrie Irving" ]
+
+p nba_roster["Cleveland Cavaliers"][2]
+# "Kyrie Irving"
