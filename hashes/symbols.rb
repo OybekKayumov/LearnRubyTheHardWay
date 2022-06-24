50a2a088b0458a5b57c16564adbdcdc7fad823f8
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-24 05:34:20 +0500

    add hash without rocket operator =>

--- hashes/symbols.rb
+++ hashes/symbols.rb
@@ -13,4 +13,27 @@ p "name".class # String
 p :name.methods.length  # 87
 p "name".methods.length # 183
 
+puts
 #todo create person hash
+person = {:name => "Tom", 
+          :age => 25, 
+          :handsome => true,
+          :languages => ['Javascript', 'Ruby', 'C++']
+        }
+p person[:name]
+p person[:handsome]
+p person[:languages]
+p person[:age]
+
+#! same, without rocket operator "=>" between
+person_new = {name: "Tom", 
+  age: 25, 
+  handsome: true,
+  languages: ['Javascript', 'Ruby', 'C++']
+}
+
+
+p person_new[:name]
+p person_new[:age]
+p person_new[:languages]
+p person_new[:handsome]
