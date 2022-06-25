f5caa47fc8e56b7a57bdb9e064701ddd47c38b36
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-25 18:08:58 +0500

    add word frequency in a sentence String

--- hashes/merge.rb
+++ hashes/merge.rb
@@ -14,8 +14,9 @@ p kitchen.merge(market)
 p kitchen 
 # {:bread=>"2 loaves", :yogurt=>"20 cans", :milk=>"100 gallons "}
 
-# mutation merge!
+#* mutation merge!
 # p kitchen.merge!(market)
+
 puts "\n\n"
 def custom_merge(hash1, hash2)
   new_hash = hash1.dup
@@ -30,3 +31,25 @@ end
 
 p custom_merge(market, kitchen)
 # {:garlic=>"3 cloves", :tomatoes=>"5 batches", :milk=>"100 gallons ", :bread=>"2 loaves", :yogurt=>"20 cans"}
+
+puts
+# todo CHALLENGE: Word Frequency in a Sentence String
+sentence = "Once upon a time in a land far far away"
+
+def word_count(string)
+  words = string.split(" ")
+  # count = {}
+  # 0 is default value, if key not exist, value will be 0 
+  count = Hash.new(0)    
+
+  # words.each do |word|
+  #   count[word] += 1  
+  # end
+  words.each { |word| count[word] += 1 }
+  
+  count
+end
+
+p word_count(sentence)
+#! "far"=>2,    "a"=>2,
+# {"Once"=>1, "upon"=>1, "a"=>2, "time"=>1, "in"=>1, "land"=>1, "far"=>2, "away"=>1}
