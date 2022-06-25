fc78948d5e8b38a0a5337537fc02d29fc0f2acc1
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-25 05:41:29 +0500

    add correct pass hashes arguments

--- hashes/hashesAsMethodArguments.rb
+++ hashes/hashesAsMethodArguments.rb
@@ -1,1 +1,31 @@
 # todo Hashes as Method Arguments
+# 1
+def calculate_total_1(price, tip, tax)
+  tax_amount = price * tax
+  tip_amount = price * tip
+  price + tip_amount + tax_amount
+end
+
+puts calculate_total_1(24.99, 0.18, 0.07) # 31.2375
+
+#! 4.690799999999999 wrong result
+puts calculate_total_1(0.18, 0.07, 24.99) 
+
+# 2 correct pass arguments
+
+bill = {price: 24.99, tip: 0.18, tax: 0.07}
+bill = {tip: 0.18, tax: 0.07, price: 24.99}
+
+def calculate_total_2(info)
+  tax_amount = info[:price] * info[:tax]
+  tip_amount = info[:price] * info[:tip]
+  info[:price] + tip_amount + tax_amount  
+end
+
+puts calculate_total_2(bill) # 31.2375
+
+puts
+# 3 example
+puts calculate_total_2(price: 24.99, tip: 0.18, tax: 0.07)      # 31.2375
+puts calculate_total_2({ price: 24.99, tip: 0.18, tax: 0.07 })  # 31.2375
+puts calculate_total_2 price: 24.99, tip: 0.18, tax: 0.07       # 31.2375
