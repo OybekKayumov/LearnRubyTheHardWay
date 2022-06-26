7ec2a27eaf614c45e0e3829d55c048d438f697da
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-26 08:24:52 +0500

    add custom start_with and end_with

--- regex/regex.rb
+++ regex/regex.rb
@@ -1,5 +1,6 @@
 # The .start_with? and .end_with? Methods
 phrase = "The Ruby Programming Language is amazing!"
+substr = "The"
 
 puts phrase.start_with?('The')    # true
 puts phrase.start_with?('The ')    # true
@@ -15,9 +16,34 @@ puts phrase.end_with?('zing!')    # true
 puts phrase.end_with?('zing')    # false
 
 puts
-def method_name
+phrase = "The Ruby Programming Language is amazing!"
+substr = "The"  # true
+substr = "the"  # false
+
+def custom_start_with(string, substring)
+  # 2
+  # string[0, substring.length] == substring ? true : false
+
+  # 3
+  string[0, substring.length] == substring
   
+  # 1
+  # if string[0, substring.length] == substring
+  #   true
+  # else 
+  #   false
+  # end
 end
 
+puts custom_start_with(phrase, substr)
 
+puts
+phrase = "The Ruby Programming Language is amazing!"
+substr = "zing!"
 
+def custom_end_with(string, substring)
+  string[-substring.length..-1] == substring
+end
+
+puts custom_end_with(phrase, substr)
+# true
