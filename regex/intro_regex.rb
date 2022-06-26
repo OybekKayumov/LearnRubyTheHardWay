19e03c33b7ea03f1db1ff4ea94256b389c74f650
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-26 13:34:43 +0500

    add scan regex

--- regex/intro_regex.rb
+++ regex/intro_regex.rb
@@ -15,3 +15,24 @@ p /m/ =~ phrase # same
 
 p phrase =~ /!/ # ! -- position ! is 40 
 p phrase =~ /./ # ! . means any characters at all -- 0 - first element Positoin iz 0 -- T
+
+p phrase =~ / / # 1st space -- 3
+
+p phrase =~ /x/ # not found  -- nill
+
+p phrase =~ /Ru/ # 4
+
+p phrase =~ /ing/ # 17 -- 1st
+
+puts
+# todo The .scan Method 
+# gives all of the matches that we're looking for
+voicemail = "I can e reached at 555-123-4567 or regexman@mail.com"
+
+p voicemail.scan(/e/) # find all 'e'
+# ["e", "e", "e", "e", "e"]
+
+p voicemail.scan(/e/).length # 5
+
+p voicemail.scan(/re/) # ["re", "re"]
+p voicemail.scan(/re/).length # 2
