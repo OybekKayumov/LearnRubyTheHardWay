6bce770ff2e5cc73d7512ec814f4c06103311d9f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-26 08:52:46 +0500

    add custom include refactored

--- regex/include.rb
+++ regex/include.rb
@@ -1,6 +1,7 @@
 # The .include? Method
 phrase = "The Ruby Programming Language is amazing!"
 search_for = "Language"
+search_for = "the"
 
 p phrase.include? "Ruby"  # true
 p phrase.include? "Language"  # true
@@ -12,12 +13,16 @@ puts
 def custom_include?(string, substring)
   length = substring.length
   string.chars.each_with_index do |char, index|
-    if string[index, length] == substring
-      return true
-    end
-  end
-  return false
+  #   if string[index, length] == substring
+  #     return true
+  #   end
+  # end
+  # return false
   
+  return true if string[index, length] == substring
+  end
+  false
 end
 
-p custom_include?(phrase, search_for) # true
+p custom_include?(phrase, search_for) # true "Language"
+p custom_include?(phrase, search_for) # false "the"
