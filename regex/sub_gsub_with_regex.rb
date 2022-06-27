a2cf00192741434032d829bd899c5b7a5d9c5665
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-27 06:50:25 +0500

    add gsub(/[-\s\(\)]/, '')

--- regex/sub_gsub_with_regex.rb
+++ regex/sub_gsub_with_regex.rb
@@ -8,3 +8,31 @@ p "wordplay".sub('w', 'sw')
 
 p "aspirin".sub('in', 'ing')
 # "aspiring"
+
+# mutate, pverwritten object  
+p word = "aspirin"
+
+puts
+p word
+# "aspirin"
+
+word.sub!('in', 'ing')
+p word
+# "aspiring"
+
+puts
+# todo gsub replace all occurences
+puts "an apple".gsub("a", '-')
+# -n -pple 
+
+puts "555 555 1234".gsub(" ", '')
+# 5555551234
+
+puts
+puts "(555)-555 1234".gsub(" ", '').gsub("(",'').gsub(")", '').gsub("-", '')
+# 5555551234
+
+puts 
+# same with regex
+puts "(555)-555 1234".gsub(/[-\s\(\)]/, '')
+# 5555551234
