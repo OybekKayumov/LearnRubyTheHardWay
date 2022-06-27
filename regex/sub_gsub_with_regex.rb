61a000a72aca579b3fd1f9483f196100608d1b83
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-27 05:55:58 +0500

    add sub(arg1, arg2)

---
+++ regex/sub_gsub_with_regex.rb
@@ -0,0 +1,10 @@
+# The .sub and .gsub Methods with Regular Expressions
+# sub only replaces the first occurrenced character
+p "whimper".sub('m', 's')
+# whisper
+
+p "wordplay".sub('w', 'sw')
+# "swordplay"
+
+p "aspirin".sub('in', 'ing')
+# "aspiring"
