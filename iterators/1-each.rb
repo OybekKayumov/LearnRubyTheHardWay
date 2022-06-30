5cd73f9bbca1e2e1eae23e55360440803f5385a8
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-30 16:13:20 +0500

    add ruby iterators - upto, downto, times

--- iterators/1-each.rb
+++ iterators/1-each.rb
@@ -16,3 +16,19 @@ end
 puts
 # use each
 rainbow.each { |color| puts color }
+
+# times
+5.times {|t| puts t}
+
+# 0
+# 1
+# 2
+# 3
+# 4
+
+puts
+# upto and downto
+5.upto(10) {|t| puts t}
+
+15.downto(10) {|t| puts t}
+
