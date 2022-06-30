6c6e2396269e90d5f3c083bc7dec9a3760745aee
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-30 16:01:17 +0500

    add ruby iterators - each

---
+++ iterators/1-each.rb
@@ -0,0 +1,18 @@
+rainbow = %w[red orange yellow green gray indogo violet]
+
+# use cicle for
+for color in rainbow
+  puts color
+end
+
+# red
+# orange
+# yellow
+# green
+# gray
+# indogo
+# violet
+
+puts
+# use each
+rainbow.each { |color| puts color }
