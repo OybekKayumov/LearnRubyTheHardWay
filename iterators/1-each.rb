2525aca409f98d1ebc99a49b464b0ab93ed43be7
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-01 08:09:25 +0500

    add ruby iterators

--- iterators/1-each.rb
+++ iterators/1-each.rb
@@ -32,3 +32,62 @@ puts
 
 15.downto(10) {|t| puts t}
 
+# Iterator            Synonym
+# each
+# each_with_index
+# map                 collect
+# select              find_all
+# reject              delete_if
+# reduce              inject
+# each_with_object
+
+puts
+# each, no mutation
+arr = [1, 2, 3, 4, 5]
+
+res = arr.each { |x| puts x ** 2 }
+p res
+1
+4
+9
+16
+25
+[1, 2, 3, 4, 5] # returns arr
+
+puts
+# each_with_index
+rainbow = %w[red orange yellow green gray indogo violet]
+rainbow.each_with_index { |color, ind| puts "#{ind}: #{color}" }
+
+# 0: red
+# 1: orange
+# 2: yellow
+# 3: green
+# 4: gray
+# 5: indogo
+# 6: violet
+
+puts 
+# map, return new collection
+res = [1, 2, 3, 4, 5].map { |x| x +1 }
+p res 
+# [2, 3, 4, 5, 6]
+
+puts
+arr = []
+[1, 2, 3, 4, 5].each { |x| arr << x * x }
+p arr 
+# [1, 4, 9, 16, 25]
+
+puts 
+# select, blocks return true or false
+p [1, 2, 3, 4, 5].select { |x| x.even? }  # [2, 4]
+
+p [1, 2, 3, 4, 5].reject { |x| x.even? }  # [1, 3, 5]
+
+puts 
+p [1, 2, 3, 4, 5].select { |x| x.even? }.map { |y| y * y }  # [4, 16]
+
+puts
+# reduce
+p [1, 2, 3, 4, 5].reduce { |m, x| m * x }# 120
