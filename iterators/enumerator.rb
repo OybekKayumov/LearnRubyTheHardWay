bf3b16eaf3c3cae4c6b7977a399274be08ef5f60
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-02 20:50:19 +0500

    add enumerable with_object

--- iterators/enumerator.rb
+++ iterators/enumerator.rb
@@ -11,3 +11,9 @@ arr = rainbow.map.with_index { |color, ind| "#{ind}: #{color}" }
 
 p arr
 # ["0: red", "1: orange", "2: yellow", "3: green", "4: gray", "5: indogo", "6: violet"]
+
+arr_new = [1, 2, 3, 4, 5].map.with_object([]) do |x, m| 
+  m << x * x if x.even?
+end
+
+p arr_new # [4, 16]
