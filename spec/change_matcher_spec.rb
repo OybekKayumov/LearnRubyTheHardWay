9b434913e4fcf9976956dc2fefbd62b45ae2d045
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-03 11:56:19 +0500

    add chanhe matcher

---
+++ spec/change_matcher_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe "change matcher" do
+  subject { [1, 2, 3] } 
+  
+  it 'checks thet a method changes object state' do
+    expect { subject.push(4) }.to change { subject.length }.from(3).to(4)
+  end  
+end
