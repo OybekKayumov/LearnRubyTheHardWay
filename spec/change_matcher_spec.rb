2abad2cd3ed0135e8492bf677efd81bcac26d797
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-03 12:16:13 +0500

    add change matcher by(1)

--- spec/change_matcher_spec.rb
+++ spec/change_matcher_spec.rb
@@ -1,7 +1,11 @@
 RSpec.describe "change matcher" do
   subject { [1, 2, 3, 4] } 
   
+  # it 'checks that a method changes object state' do
+  #   expect { subject.push(4) }.to change { subject.length }.from(3).to(4)
+  # end 
+
   it 'checks thet a method changes object state' do
-    expect { subject.push(4) }.to change { subject.length }.from(3).to(4)
+    expect { subject.push(4) }.to change { subject.length }.by(1)
   end 
 end
