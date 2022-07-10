db03b23ce1766580ab6f661108b1aaa5c6d74ae2
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-10 12:18:58 +0500

    add respond to spec methods with arguments

--- spec/respond_to_matcher_spec.rb
+++ spec/respond_to_matcher_spec.rb
@@ -26,4 +26,9 @@ RSpec.describe HotChocolate do
     expect(subject).to respond_to(:drink, :discard)
     expect(subject).to respond_to(:drink, :discard, :purchase)
   end
+
+  it 'confirms an object can respond to a method with arguments' do
+    expect(subject).to respond_to(:purchase)
+    expect(subject).to respond_to(:purchase).with(1).arguments
+  end
 end
