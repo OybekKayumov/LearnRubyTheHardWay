cd0c7c29932cdd67012eb7c3c217b30a386b0c0d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-12 16:19:42 +0500

    add copmound-expectations 'and or' spec

--- spec/compound_expectations_spec.rb
+++ spec/compound_expectations_spec.rb
@@ -8,3 +8,19 @@ RSpec.describe 25 do
 
   it { is_expected.to be_odd and be > 20 }
 end
+
+RSpec.describe 'caterpillar' do
+  it 'supports multiple matchers on a single line' do
+    
+    expect(subject).to start_with('cat').and end_with('pillar') 
+  end
+
+  it { is_expected.to start_with('cat') and end_with('pillar') }
+end
+
+RSpec.describe [:usa, :canada, :mexico] do
+  it 'supports multiple matchers on a single line' do
+    
+    expect(subject.sample).to eq(:usa).or eq(:canada).or eq(:mexico) 
+  end
+end
