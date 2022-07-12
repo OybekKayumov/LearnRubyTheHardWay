cb074915b10ac4c6f317030d40eb9d425afd6bdc
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-12 16:05:18 +0500

    add not_to method spec

--- spec/not_to_spec.rb
+++ spec/not_to_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe "not_to method" do
 
     expect('California').not_to start_with('los')
     expect('California').not_to end_with('city')
+    
+    expect(5).not_to respond_to(:length)
+    
+    expect([:a, :b, :c]).not_to include(:d)
+
+    expect{15 / 4}.not_to raise_error
+    expect{15 / 4}.not_to raise_error(NameError)
   end
 end
