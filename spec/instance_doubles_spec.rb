e038e8c192b7507e49b42cb22beb9428d59820a9
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-24 13:55:04 +0500

    add instance-double spec

--- spec/instance_doubles_spec.rb
+++ spec/instance_doubles_spec.rb
@@ -1,5 +1,5 @@
 class Person
-  def a
+  def a(seconds)
     sleep(3)
     'Hello'
   end  
@@ -10,6 +10,17 @@ RSpec.describe Person do
     it 'can implement any method' do
       person = double(a: 'Hello', b: 20)
       expect(person.a).to eq('Hello')  
+    end
+  end
+
+  describe 'instance double' do
+    it 'can only implement methods that are defined on the class' do
+      # person = instance_double(Person, a: 'Hello', b: 20) #! err
+      person = instance_double(Person, a: 'Hello')
+
+      person = instance_double(Person)
+      allow(person).to receive(:a).with(3).and_return('Hello')
+      expect(person.a(3)).to eq('Hello') 
     end
   end
 end
