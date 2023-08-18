c5a613a04c4f51c6475d7c4ce4fcc23dd32625be
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-18 15:15:14 +0500

    Add 3d edition

--- 3d-edition/ex-05.rb
+++ 3d-edition/ex-05.rb
@@ -1,20 +1,20 @@
-my_name = 'John A. Doe'
-my_age = 35   # not a lie in 2009
-my_height = 74  # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'John A. Doe'
+age = 35   # not a lie in 2009
+height = 74  # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inshes tall."
-puts "He's #{my_weight} pounds heavy."
+puts "Let's talk about #{name}."
+puts "He's #{height} inshes tall."
+puts "He's #{weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
 
 # Let's talk about John A. Doe.
 # He's 74 inshes tall.
