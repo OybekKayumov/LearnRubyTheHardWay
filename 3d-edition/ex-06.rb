33834a9785c8de39999d9350914cc00dcf960506
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-18 16:15:47 +0500

    Add 3d edition

---
+++ 3d-edition/ex-06.rb
@@ -0,0 +1,28 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of ..."
+e = "a string which a right side."
+
+puts w + e
+
+# There are 10 types of people.
+# Those who know binary and those who don't.
+# I said: There are 10 types of people..
+# I also said: 'Those who know binary and those who don't.'.
+# Isn't that joke so funny?! false
+# This is the left side of ...a string which a right side.
