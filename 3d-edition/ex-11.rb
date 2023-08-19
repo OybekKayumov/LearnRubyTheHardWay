0b89abdb209e0b7443d1c9fd0638e287a60f5a2d
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-19 15:55:05 +0500

    Add 3d edition

---
+++ 3d-edition/ex-11.rb
@@ -0,0 +1,8 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
