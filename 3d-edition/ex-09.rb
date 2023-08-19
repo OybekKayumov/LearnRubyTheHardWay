de076f39f1b0f686b5cfabacb4a0e86b30522bc6
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-19 15:16:06 +0500

    Add 3d edition

---
+++ 3d-edition/ex-09.rb
@@ -0,0 +1,30 @@
+# Here's some new strange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+There's something going on here.
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
+
+puts
+# Here are the days: Mon Tue Wed Thu Fri Sat Sun
+# Here are the months: Jan
+# Feb
+# Mar
+# Apr
+# May
+# Jun
+# Jul
+# Aug
+
+# There's something going on here.
+# With this weird quote
+# We'll be able to type as much as we like.
+# Even 4 lines if we want, or 5, or 6.
