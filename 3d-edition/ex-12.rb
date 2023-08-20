4a5d00312da17f64cfe0658c3ee694cef7b3ffa5
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-20 14:44:20 +0500

    Add 3d edition

---
+++ 3d-edition/ex-12.rb
@@ -0,0 +1,12 @@
+print "Give me a number: "
+number = gets.chomp.to_f
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_f
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
