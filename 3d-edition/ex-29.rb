6b567486ad54bb913a4acc805b50c60aaf750cca
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-25 18:55:50 +0500

    Add 3d edition

---
+++ 3d-edition/ex-29.rb
@@ -0,0 +1,21 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "Not many cats! The world is saved!"
+end
+
+if people < dogs 
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+
