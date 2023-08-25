3115c07bb37dcaef4824c0bcaebbbbbe790f0a37
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-25 19:04:56 +0500

    Add 3d edition

---
+++ 3d-edition/ex-30.rb
@@ -0,0 +1,31 @@
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people
+  puts "We should take the cars."
+elsif cars < people
+  puts "We should not take the cars."
+else
+  puts "We can't decide."
+end
+
+if trucks > cars
+  puts "That's too many trucks."
+elsif
+  puts "Maybe we could take the trucks."
+else
+  puts "We still can't decide."
+end
+
+if people > trucks 
+  puts "Alright, let's just take the trucks."
+else
+  puts "Fine, let's stay home then."
+end
+
+# Too many cats! The world is doomed!
+# The world is dry!
+# People are greater than or equal to dogs.
+# People are less than or equal to dogs.
+# People are equal to dogs.
