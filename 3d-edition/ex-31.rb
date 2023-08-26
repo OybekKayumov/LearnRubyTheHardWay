e9cf098b95d877defd023ee3c9a0c3ec3ce21b8c
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-26 05:17:01 +0500

    Add 3d edition

---
+++ 3d-edition/ex-31.rb
@@ -0,0 +1,49 @@
+puts "You enter a dark room with two doors.  Do you go through door #1 or door #2?"
+
+print "> "
+door = $stdin.gets.chomp
+
+if door == "1"
+  puts "There's a giant bear here eating a cheese cake.  What do you do?"
+  puts "1. Take the cake."
+  puts "2. Scream at the bear." 
+
+  print"> "
+
+  bear = $stdin.gets.chomp
+
+  if bear == "1"
+    puts "The bear eats your face off. Good job!"
+  elsif bear == "2"
+    puts "The bear eats your legs off. Good job!"
+  else
+    puts "Well, doing %s is probably better. Bear runs away." % bear
+  end
+
+elsif door == 2
+  puts "You stare into the endless abyss at Cthulhu's retina."
+  puts "1. Blueberries."
+  puts "2. Yellow jacket clothespins."
+  puts "3. Understanding revolvers yelling melodies."
+
+  print "> "
+  insanity = $stdin.gets.chomp
+
+  if insanity == "1" || insanity == "2"
+    puts "Your body survives powered by a mind of jello. Good job!"
+  else
+    puts "The insanity rots your eyes into a pool of muck. Good job!"
+  end
+
+else
+  puts "You stumble around and fall on a knife and die.  Good job!"
+end
+
+
+# You enter a dark room with two doors.  Do you go through door #1 or door #2?
+# > 1
+# There's a giant bear here eating a cheese cake.  What do you do?
+# 1. Take the cake.
+# 2. Scream at the bear.
+# > 2
+# The bear eats your legs off. Good job!
