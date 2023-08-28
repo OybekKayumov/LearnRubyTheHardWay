3ea22997f7c4132fb68f16300edf05ecd441f0b3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-28 14:50:02 +0500

    Add 3d edition

--- 3d-edition/ex-39.rb
+++ 3d-edition/ex-39.rb
@@ -39,19 +39,19 @@ puts stuff
 # {"name"=>"Zed", "age"=>39, "height"=>74}
 
 # create a mapping of state to abbreviation
-states = [
+states = {
   'Oregon' => 'OR',
   'Florida' => 'FL',
   'California' => 'CA',
   'New York' => 'NY',
   'Michigan' => 'MI'
-]
+}
 
 # create a basic set of states and some cities in them
 cities = {
-  'CA' => "San Francisco",
-  'MI' => "Detroit",
-  'FL' => "Jacksonville",
+  'CA' => 'San Francisco',
+  'MI' => 'Detroit',
+  'FL' => 'Jacksonville'
 }
 
 # add some more cities
@@ -65,8 +65,73 @@ puts "OR State has: #{cities['OR']}"
 
 # puts some states
 puts '-' * 10
-# puts "Michigan's abbreviation is: #{states['Michigan']}"
 puts "Michigan's abbreviation is: #{states['Michigan']}"
-
-# puts "Florida's abbreviation is: #{states['Florida']}"
 puts "Florida's abbreviation is: #{states['Florida']}"
+
+# do it by using the state then cities dict
+puts '-' * 10
+puts "Michigan has: #{cities[states['Michigan']]}"
+puts "Florida has: #{cities[states['Florida']]}"
+
+# puts every state abbreviation
+puts '-' * 10
+states.each do |state, abbrev|
+  puts "#{state} is abbreviated #{abbrev}"
+end
+
+# puts every city in state
+puts '-' * 10
+cities.each do |abbrev, city|
+  puts "#{abbrev} has the city #{city}"
+end
+
+# now do both at the same time
+puts '-' * 10
+states.each do |state, abbrev|
+  city = cities[abbrev]
+  puts "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
+
+puts '-' * 10
+# by default Ruby says "nil" when something isn't in there
+state = states['Texas']
+
+if !state
+  puts "Sorry, no Texas."
+end
+
+# default values using ||= with the nil result
+city = cities['TX']
+city ||= 'Does Not Exist'
+puts "The city for the state 'TX' is: #{city}"
+
+# ----------
+# NY State has: New York
+# OR State has: Portland
+# ----------
+# Michigan's abbreviation is: MI
+# Florida's abbreviation is: FL
+# ----------
+# Michigan has: Detroit
+# Florida has: Jacksonville
+# ----------
+# Oregon is abbreviated OR
+# Florida is abbreviated FL
+# California is abbreviated CA
+# New York is abbreviated NY
+# Michigan is abbreviated MI
+# ----------
+# CA has the city San Francisco
+# MI has the city Detroit
+# FL has the city Jacksonville
+# NY has the city New York
+# OR has the city Portland
+# ----------
+# Oregon is abbreviated OR and has city Portland
+# Florida is abbreviated FL and has city Jacksonville
+# California is abbreviated CA and has city San Francisco
+# New York is abbreviated NY and has city New York
+# Michigan is abbreviated MI and has city Detroit
+# ----------
+# Sorry, no Texas.
+# The city fpr the state 'TX' is: Does Not Exist
