ae960b31aede33a024ba4fe6870a3e7250d6f3a0
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-28 16:07:42 +0500

    Add 3d edition

--- 3d-edition/ex-40.rb
+++ 3d-edition/ex-40.rb
@@ -36,3 +36,48 @@ thing = MyStuffClass.new()
 thing.apple()               # I am classy Apples!
 puts thing.tangerine        # And now a thousand years between
 
+#! three ways to get things from things:
+
+# dict style
+# mystuff['apples']
+
+# module style
+# MyStuff.apples()
+# puts MyStuff::TANGERINE
+
+# class style
+# thing = MyStuff.new()
+# thing.apples()
+# puts thing.tangerine
+
+# todo: first class example
+class Song
+  def initialize(lyrics)
+    @lyrics = lyrics
+  end
+
+  def sing_me_a_song
+    @lyrics.each { |line| puts line }
+  end
+end
+
+puts
+happy_bday = Song.new([
+  "Happy birthday to you",
+  "I don't want to get sued",
+  "So I'll stop right there"
+])
+
+bulls_on_parade = Song.new([
+  "They rally around tha family",
+  "With pockets full of shells"
+])
+
+happy_bday.sing_me_a_song()
+bulls_on_parade.sing_me_a_song()
+
+# Happy birthday to you
+# I don't want to get sued
+# So I'll stop right there
+# They rally around tha family
+# With pockets full of shells
