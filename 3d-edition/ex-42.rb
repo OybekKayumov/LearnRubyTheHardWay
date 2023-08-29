f1dde319b0b8ab20573179d55762703fe23ab7c8
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-29 18:52:33 +0500

    Add 3d edition

--- 3d-edition/ex-42.rb
+++ 3d-edition/ex-42.rb
@@ -13,15 +13,15 @@ end
 ## Cat is-a Animal
 class Cat < Animal
   def initialize(name)
-    ## ??
+    ## cat has-a name
     @name = name
   end
 end
 
-## ??
+## Person is-a Object
 class Person
   def initialize(name)
-    ## ??
+    ## person has-a name
     @name = name
 
     ## Person has-a pet some kind
@@ -31,51 +31,52 @@ class Person
   attr_accessor :pet
 end
 
-## ??
+## Employee is-a Person
 class Employee < Person
   def initialize(name, salary)
     ## ?? hmm what is this strange magic?
+    ## argument "name" is forwarded to Person's initialize method
     super(name)
-    ## ??
+    ## Employee has-a salary
     @salary = salary
   end
 end
 
-## ??
+## Fish is-a object
 class Fish
 end
 
-## ??
+## Salmon is-a Fish
 class Salmon < Fish
 end
 
-## ??
+## Halibut is-a Fish
 class Halibut < Fish
 end
 
 ## rover is-a Dog
 rover = Dog.new("Rover")
 
-## ?? 
+## sam is-a Cat
 sam = Cat.new("Sam")
 
-## ??
+## mary is-a Person
 mary = Person.new("Mary")
 
-## ??
+## mary has-a pet sam
 mary.pet = sam
 
-## ??
+## frank is-a Employee
 frank = Employee.new("Frank", 120000)
 
-## ??
+## frank has-a pet rover
 frank.pet = rover
 
-## ??
+## flipper is-a Fish
 flipper = Fish.new()
 
-## ??
+## crouse is-a Salmon
 crouse = Salmon.new()
 
-## ??
+## harry is_a Halibut
 harry = Halibut.new()
