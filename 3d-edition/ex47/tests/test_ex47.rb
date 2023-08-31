835300735fba24b118b06cc937ac1c489e64c13e
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-31 16:51:25 +0500

    Add 3d edition

--- 3d-edition/ex47/tests/test_ex47.rb
+++ 3d-edition/ex47/tests/test_ex47.rb
@@ -16,4 +16,27 @@ class TestGame < Test::Unit::TestCase
     assert_equal({}, gold.paths)
   end
   
+  def test_room_paths()
+    center = Room.new("Center", "Test room in the center.")
+    north = Room.new("North", "Test room in the north.")
+    south = Room.new("South", "Test room in the south.")
+
+    center.add_paths({'north'=> north, 'south'=> south})
+    assert_equal(north, center.go('north'))
+    assert_equal(south, center.go('south'))
+  end
+  
+  def test_map()
+    start = Room.new("Start", "You can go west and down a hole.")
+    west = Room.new("Trees", "There are trees here, you can go east.")
+    down = Room.new("Dungeon", "It's dark down here, you can go up.")
+
+    start.add_paths({'west'=> west, 'down'=> down})
+    west.add_paths({'east'=> start})
+    down.add_paths({'up'=> start})
+
+    assert_equal(west, start.go('west'))
+    assert_equal(start, start.go('west').go('east'))
+    assert_equal(start, start.go('down').go('up'))
+  end
 end
