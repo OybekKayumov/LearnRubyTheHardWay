8136942d25c7d087842c3c52257bf99015752458
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-31 05:32:20 +0500

    Add 3d edition

---
+++ 3d-edition/ex-47/tests/test_ex47.rb
@@ -0,0 +1,18 @@
+require "ex-47/game.rb"
+require "test/unit"
+
+class TestGame < Test::Unit::TestCase
+
+  def test_sample
+    assert_equal(4, 2+2)
+  end
+
+  def test_room()
+    gold = Room.new("GoldRoom",
+      """This room has gold in it you can grab. There's a door to the north."""
+    )
+
+    assert_equal("GoldRoom", gold.name)
+    assert_equal({}, gold.paths)
+  end
+end
