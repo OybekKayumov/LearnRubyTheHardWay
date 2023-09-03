95b0859361cb567d8bd24d5fda3d8fc825b663bd
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-03 05:59:45 +0500

    Add Complete RoR Developer

---
+++ complete-ror-dev/ruby/auth.rb
@@ -0,0 +1,14 @@
+users = [
+  { username: "john", password: "pwd1" },
+  { username: "jack", password: "pwd2" },
+  { username: "arya", password: "pwd3" },
+  { username: "show", password: "pwd4" },
+  { username: "heisenberg", password: "pwd5" },
+]
+
+puts "Welcome to the authenticator"
+25.times { print "-"}
+puts 
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
+
