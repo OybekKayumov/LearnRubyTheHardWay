3d4d6947860eb4a3f5811424076230cb3714ba44
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-04 04:59:32 +0500

    Add Complete RoR Developer

--- complete-ror-dev/ruby/oop-projects/student.rb
+++ complete-ror-dev/ruby/oop-projects/student.rb
@@ -2,16 +2,24 @@
 # first name, last name, email  -> attributes
 
 class Student
-  attr_accessor :first_name, :last_name, :email, :username
+  attr_accessor :first_name, :last_name, :email, :username, :password
   # attr_reader :password
 
   # instance variables
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  # @first_name
+  # @last_name
+  # @email
+  # @username
+  # @password
   # @password = "pwd1"
+
+  def initialize(first_name, last_name, username, email, password)
+    @first_name = first_name
+    @last_name = last_name
+    @username = username
+    @email = email
+    @password = password
+  end
 
   # def first_name=(name)
   #   @first_name = name
@@ -24,26 +32,30 @@ class Student
 
   # by default
   def to_s
-    "First name: #{@first_name}"
+    "Student: #{@first_name}, #{last_name}, #{username}, #{email}"
   end
   
 
 end
 
 # create an instance of Student class
-john = Student.new
+# john = Student.new
 # puts john
 # john.first_name("John")
 # puts john
 
-john.first_name = "John"
-john.last_name = "Doe"
-john.email = "john@email.com"
-john.username = "john"
-# john.password = "pwd1"
+# john.first_name = "John"
+# john.last_name = "Doe"
+# john.email = "john@email.com"
+# john.username = "john"
+# # john.password = "pwd1"
 
-puts john.first_name
-puts john.last_name
-puts john.email
-puts john.username
-puts john.password
+# puts john.first_name
+# puts john.last_name
+# puts john.email
+# puts john.username
+# puts john.password
+
+john = Student.new("John", "Doe", "john", "john@email.com", "pwd1")
+puts john
+# Student: John, Doe, john, john@email.com
