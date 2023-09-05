d223ec373fa760abca5b7878caa6e2939492b480
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-05 16:44:11 +0500

    Add the-well-grounded-rubyist-third-edition

--- well-grounded-rubyist/chapter-2/object.rb
+++ well-grounded-rubyist/chapter-2/object.rb
@@ -7,3 +7,13 @@ def obj.talk
 end
 
 puts obj.talk
+
+# I am an pbject.
+# (Do you object?)
+
+# Celsius-to-Fahrenheit converter
+def obj.c2f(c)
+  c * 9.0 / 5 + 32
+end
+
+puts obj.c2f(100)   # 212.0
