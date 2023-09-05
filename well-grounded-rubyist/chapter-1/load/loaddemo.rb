61099e930178fa27f438f7338917d91174d68225
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-05 16:14:25 +0500

    Add the-well-grounded-rubyist-third-edition

---
+++ well-grounded-rubyist/chapter-1/load/loaddemo.rb
@@ -0,0 +1,8 @@
+puts "This is the first (master) program file."
+load "loadee.rb"
+puts "And back again to the first file"
+
+
+# This is the first (master) program file.
+# > This is the second file.
+# And back again to the first file
