1169bccc05fb86acf438d1699b79d18e69a70d31
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-09 04:54:57 +0500

    Add class

--- well-grounded-rubyist/chapter-3/class.rb
+++ well-grounded-rubyist/chapter-3/class.rb
@@ -9,3 +9,38 @@ end
 ticket = Ticket.new
 puts ticket.event
 # Can't really be specified yet...
+
+# Overriding methods
+class C
+  def m
+    puts "First definition of method m"
+  end
+  
+  def m
+    puts "Seconf definition of method m"
+  end
+end
+
+C.new.m
+# Seconf definition of method m
+
+# Reopening classes
+class C
+  def x
+  end
+end
+
+class C
+  def y
+  end
+end
+
+# same result
+class C
+  def x
+  end
+
+  def y
+  end
+end
+
