8e47c9c1d2c59f42e0c550a71bddc0412aa2e11d
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-08 05:04:57 +0500

    Add scope - local

--- well-grounded-rubyist/chapter-2/scope.rb
+++ well-grounded-rubyist/chapter-2/scope.rb
@@ -41,3 +41,20 @@ say_goodbye_2
 # Ответ заключается в том, что переменные в Ruby (за некоторыми исключениями, особенно переменными, привязанными к целым числам) не содержат значений объектов. str не содержит "Hello". Скорее, str содержит ссылку на строковый объект. Это строковый объект, который содержит буквы, составляющие "Hello".
 
 # В присваивании с именем переменной слева и объектом справа переменная получает ссылку на объект. При присвоении одной переменной другой ( abc = str) переменная слева получает копию ссылки, хранящейся в переменной справа, в результате чего обе переменные теперь содержат ссылки на один и тот же объект.
+
+# !
+# x = 1
+# x++     # No such operator
+# The reason is that due to the immediate presence of 1 in x, x++ would be like 1++, which means you’d be changing the number 1 to the number 2—and that makes no sense.
+
+puts "----------------"
+# References in variable assignment and reassignment
+str = "Hi"
+abc = str
+str = "Goodbbye !"
+puts str
+puts abc
+
+# Goodbbye !
+# Hi
+
