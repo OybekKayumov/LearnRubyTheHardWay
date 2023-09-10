77aedb91f1a8cf5fc1298eb67b92b539941cb2f4
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-10 05:17:21 +0500

    Add class

--- well-grounded-rubyist/chapter-3/class.rb
+++ well-grounded-rubyist/chapter-3/class.rb
@@ -44,3 +44,33 @@ class C
   end
 end
 
+# Instance variables and object state
+ticket = Object.new
+def ticket.price
+  117.50
+end
+
+# The instance variable enables individual objects to remember state. 
+
+#  Instance variable names always start with a single @ (at sign). This enables you to recognize an instance variable at a glance.
+#  Instance variables are only visible to the object to which they belong. (Being “visible to an object” has a technical definition having to do with the default object self, which you’ll see more about in chapter 5.)
+#  An instance variable initialized in one method inside a class can be used by any instance method defined within that class.
+
+class Person
+  def set_name(string)
+    puts "Setting person's name..."
+    @name = string
+  end
+  
+  def get_name
+    puts "Returning person's name..."
+    @name
+  end
+end
+
+joe = Person.new
+joe.set_name("Joe")
+puts joe.get_name
+# Setting person's name...
+# Returning person's name...
+# Joe
