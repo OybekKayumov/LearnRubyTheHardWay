bc9f32cf8908391f45a740374ed475d8de4361ab
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-10 05:32:33 +0500

    Add class

--- well-grounded-rubyist/chapter-3/class.rb
+++ well-grounded-rubyist/chapter-3/class.rb
@@ -74,3 +74,34 @@ puts joe.get_name
 # Setting person's name...
 # Returning person's name...
 # Joe
+
+
+# Initializing an object with state
+class Ticket
+  def initialize(venue, date)
+    # puts "Creating a new ticket!"
+    @venue = venue
+    @date = date
+  end
+
+  def venue
+    @venue
+  end
+  
+  def date
+    @date
+  end
+end
+
+th = Ticket.new("Town Hall", "2013-11-12")
+cc = Ticket.new("Convention Center", "2014-12-13")
+
+puts "We've created two tickets."
+puts "The first is for a #{th.venue} event on #{th.date}."
+puts "The second is for an event on #{cc.date} at #{cc.venue}."
+
+# We've created two tickets.
+# The first is for a Town Hall event on 2013-11-12.
+# The second is for an event on 2014-12-13 at Convention Center.
+
+# Setter methods
