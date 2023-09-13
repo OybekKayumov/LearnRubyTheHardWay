0999f785ff413b154398842ba9b4b0f9945f9a99
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-13 11:09:39 +0500

    Add modules

--- well-grounded-rubyist/chapter-4/3-module-inheritance.rb
+++ well-grounded-rubyist/chapter-4/3-module-inheritance.rb
@@ -15,3 +15,27 @@ end
 obj = D.new
 obj.report
 # 'report' method in module M
+
+# Two same-named methods on a single search path
+module InterestBearing
+  def calculate_interest
+    puts "Placeholder! We're in module InterestBearing"
+  end
+end
+
+class BankAccount
+  include InterestBearing
+
+  def calculate_interest
+    puts "Placeholder! We're in class BankAccount."
+    puts "And we're overriding the calculate_interest method..."
+    puts "which was defined in the InterestBearing module."
+  end
+end
+
+acc = BankAccount.new
+acc.calculate_interest
+
+# Placeholder! We're in class BankAccount.
+# And we're overriding the calculate_interest method...
+# which was defined in the InterestBearing module.
