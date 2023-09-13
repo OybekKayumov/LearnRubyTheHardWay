0228a17392737f97369b43ffe337c7a5a8f0b096
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-13 10:48:13 +0500

    Add modules

---
+++ well-grounded-rubyist/chapter-4/3-module-inheritance.rb
@@ -0,0 +1,17 @@
+module M
+  def report
+    puts "'report' method in module M" 
+  end
+end
+
+class C
+  include M
+end
+
+class D < C
+  
+end
+
+obj = D.new
+obj.report
+# 'report' method in module M
