3de0a268be547028838af173fde7cea89a71a99c
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-14 11:17:07 +0500

    Add modules

---
+++ well-grounded-rubyist/chapter-4/5-super.rb
@@ -0,0 +1,25 @@
+module M
+  def report
+    puts "'report' method in module M"
+  end
+end
+
+class C
+  include M
+
+  def report
+    puts "'report' method in class C"
+    puts "About to trigger the next higher-up report method..."
+    super
+    puts "Back from the 'super' call."
+  end
+end
+
+c = C.new
+c.report
+
+# 'report' method in class C
+# About to trigger the next higher-up report method...
+# 'report' method in module M
+# Back from the 'super' call.
+
