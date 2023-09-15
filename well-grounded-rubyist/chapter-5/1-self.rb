de0fd98fdfc67975fe247648b79560c7fd42ee30
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-15 12:07:15 +0500

    Add self and scope

--- well-grounded-rubyist/chapter-5/1-self.rb
+++ well-grounded-rubyist/chapter-5/1-self.rb
@@ -90,3 +90,75 @@ c = C.new
 c.y
 # This is method 'y', about to call x without a dot.
 # This is method 'x'
+
+# Composing whole name from values, using method calls on implicit self
+class Person
+  attr_accessor :first_name, :middle_name, :last_name
+
+  def whole_name
+    n = first_name + " "
+    n << "#{middle_name} " if middle_name
+    n << last_name
+  end
+end
+
+puts "5.2------------"
+david = Person.new
+david.first_name = "David"
+david.last_name = "Black"
+puts "David's whole name: #{david.whole_name}"
+david.middle_name = "Alan"
+puts "David's new whole name: #{david.whole_name}"
+# David's whole name: David Black
+# David's new whole name: David Alan Black
+
+# Resolving instance variables through self
+class A
+  def set_v
+    @v = "I am a instance variable and I belong to any instance of A."
+  end
+  
+  def show_var
+    puts @v
+  end
+  
+  def self.set_v
+    @v = "I am an instance variable and I belong to A."
+  end
+end
+
+puts
+A.set_v
+a = A.new
+a.set_v
+a.show_var
+# I am a instance variable and I belong to any instance of A.
+
+# Demonstrating the relationship between instance variables and self
+puts "5.3---------------"
+class B
+  puts "Just inside class definition block. Here's self:"
+  p self
+  @v = "I am an instance variable at the top of a class body."
+  puts = "And here's the instance variable @v, belonging to #{self}:"
+  p @v
+
+  def show_var
+    puts "Inside an instance method definition block. Here's self:"
+    p self
+    puts "And here's the instance variable @v, belonging to #{self}:"
+    p @v
+  end
+end
+
+
+b = B.new
+b.show_var
+
+# Just inside class definition block. Here's self:
+# B
+# "I am an instance variable at the top of a class body."
+# Inside an instance method definition block. Here's self:
+# #<B:0x00000168a43f6070>
+# And here's the instance variable @v, belonging to #<B:0x00000168a43f6070>:
+# nil
