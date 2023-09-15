09f67e3fed696e9985dcf23c0b8f23be664daa59
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-15 11:21:00 +0500

    Add self and scope

--- well-grounded-rubyist/chapter-5/1-self.rb
+++ well-grounded-rubyist/chapter-5/1-self.rb
@@ -61,3 +61,32 @@ puts
 C.y
 # Class method of class C
 # self: C
+
+puts
+class C
+  def C.no_dot
+    puts "As long as self is C, you can call this method with no dot"
+  end
+  
+  no_dot
+end
+puts
+C.no_dot
+# As long as self is C, you can call this method with no dot
+# As long as self is C, you can call this method with no dot
+
+class C
+  def x
+    puts "This is method 'x'"
+  end
+  
+  def y
+    puts "This is method 'y', about to call x without a dot."
+    x
+  end
+end
+
+c = C.new
+c.y
+# This is method 'y', about to call x without a dot.
+# This is method 'x'
