1ae0a962dd2cfebab39350e390cc71ac9f2c480f
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-16 16:01:32 +0500

    Add local scope and vars

--- well-grounded-rubyist/chapter-5/baker.rb
+++ well-grounded-rubyist/chapter-5/baker.rb
@@ -36,3 +36,52 @@ class Baker
   def stir_batter
   end
 end
+
+b = Baker.new
+# b.add_egg : private method `add_egg' called for
+
+class Dog
+  attr_reader :age, :dog_years
+
+  def dog_years=(years)
+    @dog_years = years
+  end
+
+  def age=(years)
+    @age = years
+    self.dog_years = years * 7
+  end
+  
+  private :dog_years=
+end
+
+rover = Dog.new
+rover.age = 10
+puts "Rover is #{rover.dog_years} in dog years."
+
+# Rover is 70 in dog years.
+
+# Example of a protected method and its use
+class C
+  def initialize(n)
+    @n = n
+  end
+
+  def n
+    @n
+  end
+
+  def compare(c)
+    if c.n > n
+      puts "The other object's n is bigger."
+    else
+      puts "The other object's n is the same or smaller."
+    end
+  end
+
+  protected :n
+end
+
+c1 = C.new(100)
+c2 = C.new(101)
+c1.compare(c2)   # The other object's n is bigger.
