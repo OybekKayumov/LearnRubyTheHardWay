5e662368608b9e68fd39f1dea9f42ed1c4204438
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-18 11:36:19 +0500

    Add condotions and loops

--- well-grounded-rubyist/chapter-6/1-if-else.rb
+++ well-grounded-rubyist/chapter-6/1-if-else.rb
@@ -17,3 +17,36 @@ end
 puts
 array = [1 ,2 ,3 ,4 ,5]
 p array.map { |n| n * 10 }
+
+puts
+5.times { puts "Writing this 5 times!" }
+
+10.times { |i| puts "I'm on iteration #{i}!" }
+
+# yield at work
+class Integer
+  def my_times
+    c = 0
+    puts "c = 0"
+    puts "until c == #{self}..."
+    until c == self
+      yield c
+      c += 1
+    end
+
+    self
+  end
+end
+
+puts
+ret = 5.my_times { |i| puts "I'm on iteration #{i}!" }
+puts ret
+
+# c = 0
+# until c == 5...
+# I'm on iteration 0!
+# I'm on iteration 1!
+# I'm on iteration 2!
+# I'm on iteration 3!
+# I'm on iteration 4!
+# 5
